2f55e2d53c8591f570d50a0dad17c14f2d34b47b
PeGiannOS <Bamelassamelas@gmail.com>
2015-04-05 21:56:11 +0300

    Added interface prototype basic methods

--- app/src/main/java/Database/Database.java
+++ app/src/main/java/Database/Database.java
@@ -8,6 +8,15 @@ public interface Database {
     //Commit new sample from heart beat monitor to Database
     public void commitNewHBSample(float sampleValue);
 
-    public int[] getData(int a);
-
+	//Get (x = num) Data from beginning
+    public int[] getData(int num);
+	//Get Data from start to end
+	public int[] getDataRange(int start,int end);
+	//Get Data Up to end ,where end is index
+	public int[] getDataBefore(int end);
+	//Get Data from start up to database length,where start is index
+	public int[] getDataAfter(int start);
+	//Get Specific Data. p.x: 0,5,234,543,ktlp
+	public int[] getSpecificData(int... pos);
+	
 }
