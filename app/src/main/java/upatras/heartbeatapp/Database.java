f004bafab1a00a452e98fe6636e4d9d43857310d
George Nicolson <aggronfg@yahoo.com>
2015-04-05 20:59:44 +0300

    interface Database with method to commit new sample from heart beat monitor

---
+++ app/src/main/java/upatras/heartbeatapp/Database.java
@@ -0,0 +1,11 @@
+package upatras.heartbeatapp;
+
+/**
+ * Created by George on 4/5/2015.
+ */
+public interface Database {
+
+    //Commit new sample from heart beat monitor to Database
+    public void commitNewHBSample(float sampleValue);
+
+}
