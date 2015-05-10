43fd7cbb85b826d9a27fe7b7be57c747760fedde
George Nicolson <aggronfg@yahoo.com>
2015-05-10 23:03:10 +0300

    Thread to recieve data

---
+++ app/src/main/java/bluetooth/ConnectThread.java
@@ -0,0 +1,61 @@
+package bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+
+
+class ConnectThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+    private BluetoothAdapter mBluetoothAdapter;
+
+    public ConnectThread(BluetoothDevice device, BluetoothAdapter ba) {
+        // Use a temporary object that is later assigned to mmSocket,
+        // because mmSocket is final
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+        mBluetoothAdapter = ba;
+
+        // Get a BluetoothSocket to connect with the given BluetoothDevice
+        try {
+            // MY_UUID is the app's UUID string, also used by the server code
+            tmp = device.createRfcommSocketToServiceRecord(mmDevice.getUuids()[0].getUuid());
+        } catch (IOException e) { }
+        mmSocket = tmp;
+    }
+
+    public void run() {
+        // Cancel discovery because it will slow down the connection
+        mBluetoothAdapter.cancelDiscovery();
+
+        try {
+            // Connect the device through the socket. This will block
+            // until it succeeds or throws an exception
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and get out
+            try {
+                mmSocket.close();
+            } catch (IOException closeException) { }
+            return;
+        }
+
+        // Do work to manage the connection (in a separate thread)
+        manageConnectedSocket(mmSocket);
+    }
+
+
+    void manageConnectedSocket(BluetoothSocket mmSocket){
+        ReadBytesThread rbt = new ReadBytesThread(mmSocket);
+    }
+
+    /** Will cancel an in-progress connection, and close the socket */
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) { }
+    }
+}
